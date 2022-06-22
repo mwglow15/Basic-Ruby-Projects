bcc3219522666973e592c3d1d73400061ea14002
mattwg <mattwglowacki@gmail.com>
2022-06-21 20:26:07 -0400

    Indentation

--- sub-strings/sub-strings.rb
+++ sub-strings/sub-strings.rb
@@ -1,14 +1,14 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substring (phrase, dict)
-  count = Hash.new(0)
-  dict.each do |dict_word|
-    num =  phrase.downcase.scan(dict_word).length
-    if num > 0 then
-      count[dict_word] = num
+    count = Hash.new(0)
+    dict.each do |dict_word|
+        num =  phrase.downcase.scan(dict_word).length
+        if num > 0 then
+            count[dict_word] = num
+        end
     end
-  end
-  count
+    count
 end
 
 puts substring("Howdy partner, sit down! How's it going?", dictionary)
